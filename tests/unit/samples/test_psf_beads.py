cd35314d07383c43b6ba3c7b4559945875d0255a
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2024-02-14 07:48:46 +0100

    Added valid beads test

--- tests/unit/samples/test_psf_beads.py
+++ tests/unit/samples/test_psf_beads.py
@@ -1,5 +1,6 @@
 import pytest
 from hypothesis import HealthCheck, given, settings
+from hypothesis import strategies as st
 
 from microscopemetrics.samples import psf_beads
 from microscopemetrics.strategies import strategies as st_mm
@@ -24,6 +25,40 @@ def test_psf_beads_analysis_run(dataset):
     assert dataset["unprocessed_analysis"].run()
     assert dataset["unprocessed_analysis"].processed
     assert dataset["unprocessed_analysis"].output
+
+
+@pytest.mark.analysis
+@given(
+    st_mm.st_psf_beads_dataset(
+        psf_beads_test_data=st_mm.st_psf_beads_test_data(
+            nr_edge_beads=st.just(0),
+            nr_out_of_focus_beads=st.just(0),
+            nr_clustering_beads=st.just(0),
+        )
+    )
+)
+@settings(max_examples=1, suppress_health_check=[HealthCheck.too_slow], deadline=20000)
+def test_psf_beads_analysis_valid_beads(dataset):
+    psf_beads_dataset = dataset["unprocessed_analysis"]
+    expected_output = dataset["expected_output"]
+    psf_beads_dataset.run()
+
+    assert psf_beads_dataset.processed
+
+    assert len(psf_beads_dataset.output.analyzed_bead_centroids) == len(
+        psf_beads_dataset.input.psf_beads_images
+    )
+
+
+"""
+        "valid_bead_positions": valid_bead_positions,
+        "edge_bead_positions": edge_beads_positions,
+        "out_of_focus_bead_positions": out_of_focus_beads_positions,
+        "clustering_bead_positions": clustering_beads_positions,
+        "applied_sigmas": applied_sigmas,
+        "signal": signal,
+        "do_noise": do_noise,
+"""
 
 
 # from tests.test_utilities import get_file
