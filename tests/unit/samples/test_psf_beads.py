909da7b3603349f97ae7bd653a76abc50022d33d
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2024-02-15 21:27:41 +0100

    Added tests for axial edge beads and for intensity outliers

--- tests/unit/samples/test_psf_beads.py
+++ tests/unit/samples/test_psf_beads.py
@@ -77,6 +77,56 @@ def test_psf_beads_analysis_nr_edge_beads(dataset):
         assert measured == expected
 
 
+@pytest.mark.analysis
+@given(
+    st_mm.st_psf_beads_dataset(
+        psf_beads_test_data=st_mm.st_psf_beads_test_data(
+            c_image_shape=st.just(3),
+            nr_valid_beads=st.just(0),
+            nr_edge_beads=st.just(0),
+            nr_out_of_focus_beads=st.integers(min_value=0, max_value=10),
+            nr_clustering_beads=st.just(0),
+        )
+    )
+)
+@settings(max_examples=50, suppress_health_check=[HealthCheck.too_slow], deadline=20000)
+def test_psf_beads_analysis_nr_axial_edge_beads(dataset):
+    psf_beads_dataset = dataset["unprocessed_analysis"]
+    expected_output = dataset["expected_output"]
+    psf_beads_dataset.run()
+    assert psf_beads_dataset.processed
+
+    expected = sum(len(im["out_of_focus_bead_positions"]) for im in expected_output.values())
+
+    for measured in psf_beads_dataset.output.key_values.nr_of_beads_considered_axial_edge:
+        assert measured == expected
+
+
+@pytest.mark.analysis
+@given(
+    st_mm.st_psf_beads_dataset(
+        psf_beads_test_data=st_mm.st_psf_beads_test_data(
+            c_image_shape=st.just(3),
+            nr_valid_beads=st.just(10),
+            nr_edge_beads=st.just(0),
+            nr_out_of_focus_beads=st.just(0),
+            nr_clustering_beads=st.integers(min_value=0, max_value=2),
+        )
+    )
+)
+@settings(max_examples=50, suppress_health_check=[HealthCheck.too_slow], deadline=20000)
+def test_psf_beads_analysis_nr_intensity_outliers_beads(dataset):
+    psf_beads_dataset = dataset["unprocessed_analysis"]
+    expected_output = dataset["expected_output"]
+    psf_beads_dataset.run()
+    assert psf_beads_dataset.processed
+
+    expected = sum(len(im["clustering_bead_positions"]) for im in expected_output.values())
+
+    for measured in psf_beads_dataset.output.key_values.nr_of_beads_considered_intensity_outlier:
+        assert measured == expected
+
+
 # from tests.test_utilities import get_file
 #
 # from microscopemetrics.samples import psf_beads
