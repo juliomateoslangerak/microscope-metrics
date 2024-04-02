52f1bcc820cf5deea69a8594144d480c4b6c978c
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2024-04-03 01:05:13 +0200

    Outputting the same datatype as the source image

--- src/microscopemetrics/samples/field_illumination.py
+++ src/microscopemetrics/samples/field_illumination.py
@@ -33,10 +33,9 @@ def _channel_intensity_map(channel: np.ndarray, map_size: int):
     intensity_map : np.ndarray
         2d np.ndarray representing the intensity map of the chosen channel.
     """
-    channel = rescale_intensity(channel, in_range=(0, channel.max()), out_range=(0, 1))
+    channel = rescale_intensity(channel, in_range=(0, channel.max()))
     zoom_factor = map_size / max(channel.shape)
-    intensity_map = scipy.ndimage.zoom(channel, zoom_factor)
-    return intensity_map.astype(np.float32)
+    return scipy.ndimage.zoom(channel, zoom_factor)
 
 
 def _image_intensity_map(image: np.ndarray, map_size: int):
