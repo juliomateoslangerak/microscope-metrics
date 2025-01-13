8df67666034c6c2d9223e420168a963123a0a13d
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2025-01-13 14:30:21 +0100

    Adding a first test for user_experiment datasets.

--- src/microscopemetrics/strategies/user_experiment.py
+++ src/microscopemetrics/strategies/user_experiment.py
@@ -30,7 +30,7 @@ def _gen_user_experiment_channel(
     target_max_intensity: float,
     do_noise: bool,
     signal: int,
-):
+) -> np.ndarray:
     # Generate the channel as float64
     channel = np.zeros(shape=(t_shape, z_shape, y_shape, x_shape), dtype="float64")
 
@@ -91,7 +91,7 @@ def _gen_user_experiment_image(
     signal: list[int],
     dtype: np.dtype,
     t_shape: int = 1,  # Added the time dimension here. Not used in the function yet
-):
+) -> np.ndarray:
     # Generate the image as float64
     image = np.zeros(shape=(t_shape, z_shape, y_shape, x_shape, c_shape), dtype="float64")
 
@@ -124,6 +124,7 @@ def st_user_experiment_test_data(
     y_image_shape=st.integers(min_value=512, max_value=1024),
     x_image_shape=st.integers(min_value=512, max_value=1024),
     c_image_shape=st.integers(min_value=1, max_value=3),
+    lines_nr=st.integers(min_value=3, max_value=7),
     dtype=st.sampled_from([np.uint8, np.uint16, np.float32]),
     do_noise=st.just(True),
     signal=st.floats(min_value=20.0, max_value=1000.0),
@@ -132,8 +133,125 @@ def st_user_experiment_test_data(
     sigma=st.floats(min_value=2.0, max_value=10.0),
     nr_orthogonal_rois=st.integers(min_value=0, max_value=3),
     nr_profile_rois=st.integers(min_value=0, max_value=3),
-):
-    pass
+) -> dict:
+    output = {
+        "images": [],
+        "lines_nr": [],
+        "orthogonal_rois": [],
+        "profile_rois": [],
+        "do_noise": [],
+        "signals": [],
+    }
+
+    c_image_shape = draw(c_image_shape)
+    z_image_shape = draw(z_image_shape)
+    y_image_shape = draw(y_image_shape)
+    x_image_shape = draw(x_image_shape)
+
+    do_noise = draw(do_noise)
+
+    dtype = draw(dtype)
+
+    for _ in range(draw(nr_images)):
+        image_target_min_intensities = [draw(target_min_intensity) for _ in range(c_image_shape)]
+        image_target_max_intensities = [draw(target_max_intensity) for _ in range(c_image_shape)]
+        image_sigma = [draw(sigma) for _ in range(c_image_shape)]
+        image_signals = [draw(signal) for _ in range(c_image_shape)]
+        image_lines_nr = [draw(lines_nr) for _ in range(c_image_shape)]
+
+        image = _gen_user_experiment_image(
+            z_shape=z_image_shape,
+            y_shape=y_image_shape,
+            x_shape=x_image_shape,
+            c_shape=c_image_shape,
+            lines_nr=image_lines_nr,
+            sigma=image_sigma,
+            target_min_intensity=image_target_min_intensities,
+            target_max_intensity=image_target_max_intensities,
+            do_noise=do_noise,
+            signal=image_signals,
+            dtype=dtype,
+        )
+
+        orthogonal_rois = []
+        for _ in range(draw(nr_orthogonal_rois)):
+            ortho_roi = st_mm_schema.st_mm_roi(
+                shapes=[
+                    st_mm_schema.st_mm_point(
+                        c=st.just(None),
+                        t=st.just(None),
+                        z=st.floats(
+                            min_value=0.0,
+                            max_value=z_image_shape - 1,
+                            exclude_min=True,
+                            exclude_max=True,
+                        ),
+                        y=st.floats(
+                            min_value=0.0,
+                            max_value=y_image_shape - 1,
+                            exclude_min=True,
+                            exclude_max=True,
+                        ),
+                        x=st.floats(
+                            min_value=0.0,
+                            max_value=x_image_shape - 1,
+                            exclude_min=True,
+                            exclude_max=True,
+                        ),
+                    )
+                ]
+            )
+            orthogonal_rois.append(ortho_roi)
+
+        profile_rois = []
+        for _ in range(draw(nr_profile_rois)):
+            profile_roi = st_mm_schema.st_mm_roi(
+                shapes=[
+                    st_mm_schema.st_mm_line(
+                        c=st.just(None),
+                        t=st.just(None),
+                        z=st.floats(
+                            min_value=0.0,
+                            max_value=z_image_shape - 1,
+                            exclude_min=True,
+                            exclude_max=True,
+                        ),
+                        x1=st.floats(
+                            min_value=0.0,
+                            max_value=x_image_shape / 2,
+                            exclude_min=True,
+                            exclude_max=True,
+                        ),
+                        x2=st.floats(
+                            min_value=x_image_shape / 2,
+                            max_value=x_image_shape - 1,
+                            exclude_min=True,
+                            exclude_max=True,
+                        ),
+                        y1=st.floats(
+                            min_value=0.0,
+                            max_value=y_image_shape / 2,
+                            exclude_min=True,
+                            exclude_max=True,
+                        ),
+                        y2=st.floats(
+                            min_value=y_image_shape / 2,
+                            max_value=y_image_shape - 1,
+                            exclude_min=True,
+                            exclude_max=True,
+                        ),
+                    )
+                ]
+            )
+
+        output["images"].append(image)
+        output["lines_nr"].append(image_lines_nr)
+        output["do_noise"].append(do_noise)
+        output["signals"].append(image_signals)
+        output["orthogonal_rois"].append(orthogonal_rois)
+        output["profile_rois"].append(profile_rois)
+
+    return output
 
 
 @st.composite
@@ -142,4 +260,47 @@ def st_user_experiment_dataset(
     unprocessed_dataset=st_mm_schema.st_mm_user_experiment_unprocessed_dataset(),
     test_data=st_user_experiment_test_data(),
 ):
-    pass
+    test_data = draw(test_data)
+    user_experiment_unprocessed_dataset = draw(unprocessed_dataset)
+
+    # the unprocessed dataset contains one empty image, we need to delete it
+    user_experiment_unprocessed_dataset.input_data.user_experiment_images = []
+
+    for i, image in enumerate(test_data.pop("images")):
+        mm_image = numpy_to_mm_image(
+            array=image,
+            name=f"UE_image_{i}",
+            channel_names=[f"Channel_{i}{j}" for j in range(image.shape[-1])],
+        )
+
+        orthogonal_rois = test_data["orthogonal_rois"][i]
+        for ortho_roi in orthogonal_rois:
+            ortho_roi.linked_references.append(mm_image.data_reference)
+
+        profile_rois = test_data["profile_rois"][i]
+        for prof_roi in profile_rois:
+            prof_roi.linked_references.append(mm_image.data_reference)
+
+        user_experiment_unprocessed_dataset.input_data.user_experiment_images.append(mm_image)
+        user_experiment_unprocessed_dataset.input_data.orthogonal_rois.extend(orthogonal_rois)
+        user_experiment_unprocessed_dataset.input_data.profile_rois.extend(profile_rois)
+
+    # Setting the bit depth to the data type of the image
+    image_dtype = {
+        a.array_data.dtype
+        for a in user_experiment_unprocessed_dataset.input_data.user_experiment_images
+    }
+    if len(image_dtype) != 1:
+        raise ValueError("All images should have the same data type")
+    image_dtype = image_dtype.pop()
+    if np.issubdtype(image_dtype, np.integer):
+        user_experiment_unprocessed_dataset.input_parameters.bit_depth = np.iinfo(image_dtype).bits
+    elif np.issubdtype(image_dtype, np.floating):
+        user_experiment_unprocessed_dataset.input_parameters.bit_depth = np.finfo(image_dtype).bits
+    else:
+        user_experiment_unprocessed_dataset.input_parameters.bit_depth = None
+
+    return {
+        "unprocessed_dataset": user_experiment_unprocessed_dataset,
+        "expected_output": test_data,
+    }
