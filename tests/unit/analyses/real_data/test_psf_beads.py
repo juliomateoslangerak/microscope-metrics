8260abc41bf0ea3ae40a90a0ac61c31806306909
Julio Mateos Langerak <julio.matoes-langerak@igh.cnrs.fr>
2025-02-14 22:22:00 +0100

    Added tests for psf_beads. Test not passing.

---
+++ tests/unit/analyses/real_data/test_psf_beads.py
@@ -0,0 +1,47 @@
+import pytest
+from tests.helper_functions import filter_dict, remove_np_pd_data
+
+from dataclasses import asdict
+
+from microscopemetrics.analyses.psf_beads import analyse_psf_beads
+from microscopemetrics_schema.datamodel import (
+    PSFBeadsDataset,
+    PSFBeadsInputParameters,
+    PSFBeadsInputData,
+    PSFBeadsOutput,
+    PSFBeadsKeyMeasurements,
+)
+
+
+@pytest.mark.parametrize(
+    "images_dataset_generator",
+    [
+        {
+            "dataset_path": "psf_beads_datasets",
+            "dataset_target_class": PSFBeadsDataset,
+            "input_parameters_target_class": PSFBeadsInputParameters,
+            "input_data_target_class": PSFBeadsInputData,
+            "input_images_field": "psf_beads_images",
+            "output_target_class": PSFBeadsOutput,
+            "key_measurements_target_class": PSFBeadsKeyMeasurements,
+        }
+    ],
+    indirect=True
+)
+def test_psf_beads(images_dataset_generator):
+    expected_output = asdict(images_dataset_generator.output)
+    images_dataset_generator.output = None
+    assert analyse_psf_beads(images_dataset_generator)
+    assert images_dataset_generator.processed
+
+    analyzed_output = asdict(images_dataset_generator.output)
+
+    analyzed_output = filter_dict(
+        expected=expected_output,
+        analyzed=analyzed_output,
+    )
+    expected_output = remove_np_pd_data(expected_output)
+    analyzed_output = remove_np_pd_data(analyzed_output)
+
+    assert analyzed_output["key_measurements"] == expected_output["key_measurements"]
+
