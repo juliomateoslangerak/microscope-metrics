c67583f4d7c7da3d658fe8e369140f0bdbb949ac
Julio Mateos Langerak <julio.matoes-langerak@igh.cnrs.fr>
2025-02-14 22:20:19 +0100

    Moved shared fixtures to conftest.py and functions to helper_functions.py

--- tests/helper_functions.py
+++ tests/helper_functions.py
@@ -1,10 +1,4 @@
-"""
-This file contains fixtures to create various invariable schema dataclasses for testing purposes.
-"""
-
 import pytest
-import microscopemetrics_schema.datamodel as mm_schema
-import datetime
 
 
 def filter_dict(expected, analyzed):
@@ -59,32 +53,3 @@ def approx_compare(expected, analyzed, rel_tol=1e-3, abs_tol=1e-3, int_tolerance
 
     return expected == analyzed  # Default exact match for other types
 
-
-@pytest.fixture(scope="session")
-def microscope() -> mm_schema.Microscope:
-    return mm_schema.Microscope(
-        name="microscope_name",
-        description="microscope_description",
-        type="WIDEFIELD",
-        manufacturer="microscope_manufacturer",
-        model="microscope_model",
-        serial_number="microscope_serial_number",
-    )
-
-
-@pytest.fixture(scope="session")
-def experimenter() -> mm_schema.Experimenter:
-    return mm_schema.Experimenter(
-        name="John Doe",
-        orcid="0123-4567-8901-2345",
-    )
-
-
-@pytest.fixture(scope="session")
-def acquisition_datetime() -> str:
-    return str(datetime.datetime.now())
-
-
-@pytest.fixture(scope="session")
-def processing_application() -> str:
-    return "Automated test run"
