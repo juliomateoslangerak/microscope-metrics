29d7b7f49b0a6c16d521b6002136c0c5e80c284a
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2025-08-30 09:43:12 +0200

    Light source power strategies and test module. First basic instantiation and run tests passing

--- src/microscopemetrics/strategies/light_source_power.py
+++ src/microscopemetrics/strategies/light_source_power.py
@@ -19,34 +19,48 @@ def _add_seconds_to_datetime(datetime: dt.datetime, seconds: float) -> dt.dateti
 
 # Strategies for Light Source Power
 @st.composite
-def st_light_source_test_data(
+def st_light_source_power_test_data(
     draw,
     light_sources=st.lists(st_mm_analyses_schema.st_mm_light_source(), min_size=1, max_size=5),
+    power_meter=st_mm_analyses_schema.st_mm_power_meter(),
     measuring_location=st.just("OBJECTIVE_FOCAL"),
     acquisition_start_datetime=st.datetimes(),
     nr_linearity_measurements=st.integers(min_value=5, max_value=10),
     linearity_interval_seconds=st.just(60.0),
     linearity_integration_time_seconds=st.just(1.0),
-    # nr_short_term_stability_measurements=st.just(300),
-    # short_term_stability_interval_seconds=st.just(1.0),
-    # short_term_stability_integration_time_seconds=st.just(0.1),
-    # nr_long_term_stability_measurements=st.just(240),
-    # long_term_stability_interval_seconds=st.just(30.0),
-    # long_term_stability_integration_time_seconds=st.just(0.1),
+    nr_short_term_stability_measurements=st.just(300),
+    short_term_stability_set_power_value=st.just(1.0),
+    short_term_stability_interval_seconds=st.just(1.0),
+    short_term_stability_integration_time_seconds=st.just(0.1),
+    nr_long_term_stability_measurements=st.just(240),
+    long_term_stability_set_power_value=st.just(1.0),
+    long_term_stability_interval_seconds=st.just(30.0),
+    long_term_stability_integration_time_seconds=st.just(0.1),
     target_intensity_mw=st.floats(min_value=10.0, max_value=100.0),
     target_intensity_std_rel=st.floats(min_value=0.01, max_value=0.1),
 ):
     power_measurements = []
+
     current_datetime = draw(acquisition_start_datetime)
     light_sources = draw(light_sources)
+
+    # We don't want to "draw" the same value for all measurements
     linearity_interval_seconds = draw(linearity_interval_seconds)
     linearity_integration_time_seconds = draw(linearity_integration_time_seconds)
     target_intensity_std_rel = draw(target_intensity_std_rel)
+    short_term_stability_interval_seconds = draw(short_term_stability_interval_seconds)
+    short_term_stability_integration_time_seconds = draw(
+        short_term_stability_integration_time_seconds
+    )
+    long_term_stability_interval_seconds = draw(long_term_stability_interval_seconds)
+    long_term_stability_integration_time_seconds = draw(
+        long_term_stability_integration_time_seconds
+    )
 
     for light_source in light_sources:
-        power_meter = draw(st_mm_analyses_schema.st_mm_power_meter())
-        location = draw(measuring_location)
-        target_intensity_mw = draw(target_intensity_mw)
+        _power_meter = draw(power_meter)
+        _measuring_location = draw(measuring_location)
+        _target_intensity_mw = draw(target_intensity_mw)
 
         # Generating linearity measurements
         set_power_values = np.linspace(0.0, 1.0, draw(nr_linearity_measurements))
@@ -57,14 +71,14 @@ def st_light_source_test_data(
                     loc=set_power_value,
                     scale=target_intensity_std_rel,
                 )
-                * target_intensity_mw,
+                * _target_intensity_mw,
             )
             power_measurements.append(
                 {
                     "acquisition_datetime": current_datetime,
                     "light_source": light_source,
-                    "measurement_device": power_meter,
-                    "measuring_location": location,
+                    "measurement_device": _power_meter,
+                    "measuring_location": _measuring_location,
                     "power_set_point": set_power_value,
                     "power_mw": measured_power,
                     "integration_time_seconds": linearity_integration_time_seconds,
@@ -74,7 +88,59 @@ def st_light_source_test_data(
                 current_datetime, linearity_interval_seconds
             )
 
-    return power_measurements
+        # Generating short term stability measurements
+        set_power_value = draw(short_term_stability_set_power_value)
+        for _ in range(draw(nr_short_term_stability_measurements)):
+            measured_power = max(
+                0.0,
+                np.random.normal(
+                    loc=set_power_value,
+                    scale=target_intensity_std_rel,
+                )
+                * _target_intensity_mw,
+            )
+            power_measurements.append(
+                {
+                    "acquisition_datetime": current_datetime,
+                    "light_source": light_source,
+                    "measurement_device": _power_meter,
+                    "measuring_location": _measuring_location,
+                    "power_set_point": set_power_value,
+                    "power_mw": measured_power,
+                    "integration_time_seconds": short_term_stability_integration_time_seconds,
+                }
+            )
+            current_datetime = _add_seconds_to_datetime(
+                current_datetime, short_term_stability_interval_seconds
+            )
+
+        # Generating long term stability measurements
+        set_power_value = draw(long_term_stability_set_power_value)
+        for _ in range(draw(nr_long_term_stability_measurements)):
+            measured_power = max(
+                0.0,
+                np.random.normal(
+                    loc=set_power_value,
+                    scale=target_intensity_std_rel,
+                )
+                * _target_intensity_mw,
+            )
+            power_measurements.append(
+                {
+                    "acquisition_datetime": current_datetime,
+                    "light_source": light_source,
+                    "measurement_device": _power_meter,
+                    "measuring_location": _measuring_location,
+                    "power_set_point": set_power_value,
+                    "power_mw": measured_power,
+                    "integration_time_seconds": long_term_stability_integration_time_seconds,
+                }
+            )
+            current_datetime = _add_seconds_to_datetime(
+                current_datetime, long_term_stability_interval_seconds
+            )
+
+    return {"input_data_power_measurements": power_measurements}
 
 
 @st.composite
@@ -84,63 +150,9 @@ def st_light_source_power_dataset(
     test_data=st_light_source_power_test_data(),
 ):
     test_data = draw(test_data)
-    field_illumination_unprocessed_dataset = draw(unprocessed_dataset)
-
-    field_illumination_unprocessed_dataset.input_data.field_illumination_images = [
-        numpy_to_mm_image(
-            array=image,
-            name=f"FI_image_{i}",
-            channel_names=[f"Channel_{i}{j}" for j in range(image.shape[-1])],
-        )
-        for i, image in enumerate(test_data.pop("images"))
+    light_source_power_dataset = draw(unprocessed_dataset)
+    light_source_power_dataset.input_data.power_measurements = test_data[
+        "input_data_power_measurements"
     ]
 
-    # Setting the bit depth to the data type of the image
-    image_dtype = {
-        a.array_data.dtype
-        for a in field_illumination_unprocessed_dataset.input_data.field_illumination_images
-    }
-    if len(image_dtype) != 1:
-        raise ValueError("All images should have the same data type")
-    image_dtype = image_dtype.pop()
-    if np.issubdtype(image_dtype, np.integer):
-        field_illumination_unprocessed_dataset.input_parameters.bit_depth = np.iinfo(
-            image_dtype
-        ).bits
-    elif np.issubdtype(image_dtype, np.floating):
-        field_illumination_unprocessed_dataset.input_parameters.bit_depth = np.finfo(
-            image_dtype
-        ).bits
-    else:
-        field_illumination_unprocessed_dataset.input_parameters.bit_depth = None
-
-    return {
-        "unprocessed_dataset": field_illumination_unprocessed_dataset,
-        "expected_output": test_data,
-    }
-
-
-@st.composite
-def st_field_illumination_table(
-    draw,
-    nr_rows=st.integers(min_value=1, max_value=50),
-):
-    nr_rows = draw(nr_rows)
-    columns = [
-        "bottom_center_intensity_mean",
-        "bottom_center_intensity_ratio",
-        "channel_nr",
-    ]
-    table = []
-    for _ in range(nr_rows):
-        dataset = draw(st_field_illumination_dataset())["unprocessed_dataset"]
-        dataset.run()
-        if dataset.processed:
-            key_values = {col: getattr(dataset.output.key_values, col) for col in columns}
-        else:
-            continue
-        table.append(key_values)
-
-    table = [pd.DataFrame(d) for d in table]
-
-    return pd.concat(table, ignore_index=True)
+    return {"unprocessed_dataset": light_source_power_dataset, "expected_output": test_data}
--- tests/unit/analyses/syntheric_data/test_light_source_power.py
+++ tests/unit/analyses/syntheric_data/test_light_source_power.py
@@ -4,178 +4,27 @@ from hypothesis import strategies as st
 from microscopemetrics_schema import datamodel as mm_schema
 
 from microscopemetrics.analyses import light_source_power
-from microscopemetrics.strategies import (
-    st_field_illumination_dataset,
-    st_field_illumination_test_data,
+from microscopemetrics.strategies.light_source_power import (
+    st_light_source_power_dataset,
+    st_light_source_power_test_data,
 )
 
 
-@given(st_field_illumination_dataset())
+@given(st_light_source_power_dataset())
 @settings(max_examples=1)
-def test_field_illumination_analysis_instantiation(dataset):
+def test_light_source_power_analysis_instantiation(dataset):
     dataset = dataset["unprocessed_dataset"]
-    assert isinstance(dataset, mm_schema.FieldIlluminationDataset)
+    assert isinstance(dataset, mm_schema.LightSourcePowerDataset)
     assert dataset.name
     assert dataset.description
     assert dataset.microscope
     assert dataset.input_parameters
 
 
-@given(st_field_illumination_dataset())
+@given(st_light_source_power_dataset())
 @settings(max_examples=1)
-def test_field_illumination_analysis_run(dataset):
+def test_light_source_power_analysis_run(dataset):
     dataset = dataset["unprocessed_dataset"]
     assert not dataset.processed
-    assert field_illumination.analyse_field_illumination(dataset)
+    assert light_source_power.analyse_light_source_power(dataset)
     assert dataset.processed
-
-
-@given(
-    st_field_illumination_dataset(
-        test_data=st_field_illumination_test_data(
-            center_y_relative=st.floats(min_value=-0.0, max_value=0.6),
-            center_x_relative=st.floats(min_value=-0.0, max_value=0.6),
-        )
-    )
-)
-def test_field_illumination_analysis_centers_geometric(dataset):
-    field_illumination_dataset = dataset["unprocessed_dataset"]
-    expected_output = dataset["expected_output"]
-    field_illumination.analyse_field_illumination(field_illumination_dataset)
-
-    measured_centers = list(
-        zip(
-            field_illumination_dataset.output.key_measurements.center_geometric_y_relative,
-            field_illumination_dataset.output.key_measurements.center_geometric_x_relative,
-        )
-    )
-    expected_centers = list(
-        zip(
-            [e_c for im in expected_output["centers_generated_y_relative"] for e_c in im],
-            [e_c for im in expected_output["centers_generated_x_relative"] for e_c in im],
-        )
-    )
-    note(f"Expected output: {expected_output}")
-    note(
-        f"Input params: "
-        f"bit_depth: {field_illumination_dataset.input_parameters.bit_depth}"
-        f"saturation_threshold: {field_illumination_dataset.input_parameters.saturation_threshold}"
-        f"sigma: {field_illumination_dataset.input_parameters.sigma}"
-    )
-
-    for measured_c, expected_c in zip(measured_centers, expected_centers):
-        assert measured_c[0] == pytest.approx(expected_c[0], abs=0.2)
-        assert measured_c[1] == pytest.approx(expected_c[1], abs=0.2)
-
-
-@given(
-    st_field_illumination_dataset(
-        test_data=st_field_illumination_test_data(
-            center_y_relative=st.floats(min_value=-0.0, max_value=0.6),
-            center_x_relative=st.floats(min_value=-0.0, max_value=0.6),
-        )
-    )
-)
-def test_field_illumination_analysis_centers_of_mass(dataset):
-    field_illumination_dataset = dataset["unprocessed_dataset"]
-    expected_output = dataset["expected_output"]
-    field_illumination.analyse_field_illumination(field_illumination_dataset)
-
-    measured_centers_weighted = list(
-        zip(
-            field_illumination_dataset.output.key_measurements.center_of_mass_y_relative,
-            field_illumination_dataset.output.key_measurements.center_of_mass_x_relative,
-        )
-    )
-    expected_centers = list(
-        zip(
-            [e_c for im in expected_output["centers_generated_y_relative"] for e_c in im],
-            [e_c for im in expected_output["centers_generated_x_relative"] for e_c in im],
-        )
-    )
-    note(f"Expected output: {expected_output}")
-    note(
-        f"Input params: "
-        f"bit_depth: {field_illumination_dataset.input_parameters.bit_depth}"
-        f"saturation_threshold: {field_illumination_dataset.input_parameters.saturation_threshold}"
-        f"sigma: {field_illumination_dataset.input_parameters.sigma}"
-    )
-
-    for measured_c_w, expected_c in zip(measured_centers_weighted, expected_centers):
-        assert measured_c_w[0] == pytest.approx(expected_c[0], abs=0.2)
-        assert measured_c_w[1] == pytest.approx(expected_c[1], abs=0.2)
-
-
-@given(st_field_illumination_dataset())
-def test_field_illumination_analysis_max_intensity_positions(dataset):
-    field_illumination_dataset = dataset["unprocessed_dataset"]
-    expected_output = dataset["expected_output"]
-    field_illumination.analyse_field_illumination(field_illumination_dataset)
-
-    measured_max_intensity_positions = list(
-        zip(
-            field_illumination_dataset.output.key_measurements.max_intensity_pos_y_relative,
-            field_illumination_dataset.output.key_measurements.max_intensity_pos_x_relative,
-        )
-    )
-    expected_centers = list(
-        zip(
-            [e_c for im in expected_output["centers_generated_y_relative"] for e_c in im],
-            [e_c for im in expected_output["centers_generated_x_relative"] for e_c in im],
-        )
-    )
-    note(f"Expected output: {expected_output}")
-    note(
-        f"Input params: "
-        f"bit_depth: {field_illumination_dataset.input_parameters.bit_depth}"
-        f"saturation_threshold: {field_illumination_dataset.input_parameters.saturation_threshold}"
-        f"sigma: {field_illumination_dataset.input_parameters.sigma}"
-    )
-
-    for measured_m_i, expected_c in zip(measured_max_intensity_positions, expected_centers):
-        assert measured_m_i[0] == pytest.approx(expected_c[0], abs=0.05)
-        assert measured_m_i[1] == pytest.approx(expected_c[1], abs=0.05)
-
-
-@given(st_field_illumination_dataset())
-def test_field_illumination_analysis_centers_fitted(dataset):
-    field_illumination_dataset = dataset["unprocessed_dataset"]
-    expected_output = dataset["expected_output"]
-    field_illumination.analyse_field_illumination(field_illumination_dataset)
-
-    measured_centers_fitted = list(
-        zip(
-            field_illumination_dataset.output.key_measurements.center_fitted_y_relative,
-            field_illumination_dataset.output.key_measurements.center_fitted_x_relative,
-        )
-    )
-    expected_centers = list(
-        zip(
-            [e_c for im in expected_output["centers_generated_y_relative"] for e_c in im],
-            [e_c for im in expected_output["centers_generated_x_relative"] for e_c in im],
-        )
-    )
-    note(f"Expected output: {expected_output}")
-    note(
-        f"Input params: "
-        f"bit_depth: {field_illumination_dataset.input_parameters.bit_depth}"
-        f"saturation_threshold: {field_illumination_dataset.input_parameters.saturation_threshold}"
-        f"sigma: {field_illumination_dataset.input_parameters.sigma}"
-    )
-
-    for measured_m_i, expected_c in zip(measured_centers_fitted, expected_centers):
-        assert measured_m_i[0] == pytest.approx(expected_c[0], abs=0.05)
-        assert measured_m_i[1] == pytest.approx(expected_c[1], abs=0.05)
-
-
-@given(
-    st_field_illumination_dataset(
-        test_data=st_field_illumination_test_data(
-            target_min_intensity=st.just(1.5),
-        )
-    )
-)
-def test_field_illumination_analysis_raises_saturation_error(dataset):
-    with pytest.raises(SaturationError):
-        dataset = dataset["unprocessed_dataset"]
-        field_illumination.analyse_field_illumination(dataset)
