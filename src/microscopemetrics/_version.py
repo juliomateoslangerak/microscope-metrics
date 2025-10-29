3ba72945b498013ee50e98572b42cc186f15ce0e
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2025-10-29 18:00:27 +0100

    Replace `__name__` with explicit package name for version retrieval

--- src/microscopemetrics/_version.py
+++ src/microscopemetrics/_version.py
@@ -1,7 +1,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version(__name__)
+    __version__ = version("microscopemetrics")
 except PackageNotFoundError:
     # package not installed
     __version__ = "0.0.0"
