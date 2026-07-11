d33ffdefc152b9812cb32772d6fb351e091f9ef9
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2026-07-11 14:15:09 +0200

    Refactored health check suppression configuration by centralizing into a shared variable.

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,44 +10,34 @@ from microscopemetrics_schema import datamodel as mm_schema
 
 from microscopemetrics.analyses import numpy_to_mm_image
 
+_common_suppress = [
+    HealthCheck.too_slow,
+    HealthCheck.large_base_example,
+    HealthCheck.data_too_large,
+]
+
 settings.register_profile(
     "push",
     max_examples=1,
-    suppress_health_check=[
-        HealthCheck.too_slow,
-        HealthCheck.large_base_example,
-        HealthCheck.data_too_large,
-    ],
+    suppress_health_check=_common_suppress,
     deadline=100000,
 )
 settings.register_profile(
     "pull_request",
     max_examples=50,
-    suppress_health_check=[
-        HealthCheck.too_slow,
-        HealthCheck.large_base_example,
-        HealthCheck.data_too_large,
-    ],
+    suppress_health_check=_common_suppress,
     deadline=100000,
 )
 settings.register_profile(
     "pre_release",
     max_examples=100,
-    suppress_health_check=[
-        HealthCheck.too_slow,
-        HealthCheck.large_base_example,
-        HealthCheck.data_too_large,
-    ],
+    suppress_health_check=_common_suppress,
     deadline=100000,
 )
 settings.register_profile(
     "dev",
     max_examples=1,
-    suppress_health_check=[
-        HealthCheck.too_slow,
-        HealthCheck.large_base_example,
-        HealthCheck.data_too_large,
-    ],
+    suppress_health_check=_common_suppress,
     deadline=100000,
     verbosity=Verbosity.verbose,
     print_blob=True,
@@ -55,11 +45,7 @@ settings.register_profile(
 settings.register_profile(
     "debug",
     max_examples=10,
-    suppress_health_check=[
-        HealthCheck.too_slow,
-        HealthCheck.large_base_example,
-        HealthCheck.data_too_large,
-    ],
+    suppress_health_check=_common_suppress,
     deadline=100000,
     verbosity=Verbosity.verbose,
     print_blob=True,
