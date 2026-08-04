031434fe75afa75e3115604d1c28acb2e518fe13
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2026-08-04 15:35:28 +0200

    Updated mappings to use `class_class_curie` instead of `__name__` for dataset keys.

--- src/microscopemetrics/analyses/mappings.py
+++ src/microscopemetrics/analyses/mappings.py
@@ -15,18 +15,18 @@ from microscopemetrics.analyses import (
 Mapping = namedtuple("Mapping", ["analysis_function", "sample_classes"])
 
 MAPPINGS = {
-    mm_schema.FieldIlluminationDataset.__name__: Mapping(
+    mm_schema.FieldIlluminationDataset.class_class_curie: Mapping(
         analysis_function=field_illumination.analyse_field_illumination,
         sample_classes=[
             mm_schema.FluorescentHomogeneousThinField,
             mm_schema.FluorescentHomogeneousThickField,
         ],
     ),
-    mm_schema.PSFBeadsDataset.__name__: Mapping(
+    mm_schema.PSFBeadsDataset.class_class_curie: Mapping(
         analysis_function=psf_beads.analyse_psf_beads,
         sample_classes=[mm_schema.PSFBeads],
     ),
-    mm_schema.LightSourcePowerDataset.__name__: Mapping(
+    mm_schema.LightSourcePowerDataset.class_class_curie: Mapping(
         analysis_function=light_source_power.analyse_light_source_power, sample_classes=[]
     ),
 }
